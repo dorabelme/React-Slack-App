707f6ae1c00847617f55a9a03da11106d1a6de0a
Dora Belme <contact.dorabelme@gmail.com>
2020-08-18 13:06:44 -0400

    channels component

---
+++ src/components/SidePanel/Channels.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Menu, Icon } from 'semantic-ui-react';
+
+class Channels extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            channels: []
+        }
+    }
+
+    render() {
+        const { channels } = this.state;
+
+        return (
+            <Menu.Menu style={{ paddingBottom: '2em' }}>
+                <Menu.Item>
+                    <span>
+                        <Icon name="exchange" /> CHANNELS
+                    </span>{" "}
+                    ({channels.length}) <Icon name='add' />
+                </Menu.Item>
+                {/*Channels*/}
+            </Menu.Menu>
+        );
+    }
+}
+
+export default Channels;
